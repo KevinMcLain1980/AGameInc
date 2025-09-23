48004ab37a432e4444ec5cd2c80b035176bffd78
Mee6hy <Dpalmer1996@gmail.com>
2025-09-23 01:08:41 -0400

    .

--- Cabin Rush/Assets/Scripts/WaterRise.cs
+++ Cabin Rush/Assets/Scripts/WaterRise.cs
@@ -4,11 +4,22 @@ public class WaterRise : MonoBehaviour
     [SerializeField] private float riseSpeed = 1f;
     [SerializeField] private float maxHeight = 10f;
 
+    PlayerMovement InWater;
+
     private void Update()
     {
         if (transform.position.y < maxHeight)
         {
             transform.position += Vector3.up * riseSpeed * Time.deltaTime;
+           
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            InWater.runSpeed -= Time.deltaTime;
         }
     }
 }
