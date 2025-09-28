6b46a156a9da45bdba32b36c58e8b9574ccd1191
Mee6hy <Dpalmer1996@gmail.com>
2025-09-28 15:03:54 -0400

    sprint

--- Cabin Rush/Assets/Scripts/Movement/PlayerController.cs
+++ Cabin Rush/Assets/Scripts/Movement/PlayerController.cs
@@ -7,15 +7,30 @@ public class PlayerController : MonoBehaviour, IOxygen, IRun
     [SerializeField] private PlayerStateManager playerStateManager;
     [SerializeField] int Oxygen;
     [SerializeField] int Stamina;
+    [SerializeField] int Hp;
+    [SerializeField] int speed;
+    [SerializeField] int speedMul;
     public bool IsDead => IsDead;
-    public bool IsRunning;
+    bool IsRunning = false;
 
-    private void Update()
+    int HpOrig;
+    int OxygenOrig;
+    int StaminaOrig;
+
+    void Start()
+    {
+        HpOrig = Hp;
+        OxygenOrig = Oxygen;
+        StaminaOrig = Stamina;
+    }
+
+    void Update()
     {
         if (Keyboard.current.hKey.wasPressedThisFrame)
         {
             playerStateManager.TakeDamage(10f);
         }
+        sprint();
     }
 
     public void TakeDamage(float damageAmount)
@@ -42,13 +57,20 @@ public class PlayerController : MonoBehaviour, IOxygen, IRun
     public void takeOxygen(int amount)
     {
         Oxygen -= amount;
+        updatePlayerUI();
     }
 
     public void takeStamina(int amount)
     {
         if(Input.GetButtonDown("Sprint") && Stamina > 0)
         {
+            IsRunning = true;
             Stamina -= amount;
+            updatePlayerUI();
+        }
+        else if (Input.GetButtonUp("Sprint"))
+        {
+            IsRunning = false;
         }
     }
 
@@ -58,5 +80,32 @@ public class PlayerController : MonoBehaviour, IOxygen, IRun
         r.takeStamina(2);
         yield return new WaitForSeconds(1);
         IsRunning = false;
+    }
+
+    public void updatePlayerUI()
+    {
+        GameManager.instance.playerHPBar.fillAmount = Hp / (float)HpOrig;
+        GameManager.instance.playerOxygenBar.fillAmount = Oxygen / (float)OxygenOrig;
+        if (IsRunning == true && Stamina > 0)
+        {
+            takeStamina(speedMul);
+            GameManager.instance.playerStaminaBar.fillAmount = Stamina / (float)StaminaOrig;
+            
+        }
+    }
+
+    void sprint()
+    {
+        if (Input.GetButtonDown("Sprint"))
+        {
+            speed *= speedMul;
+            IsRunning = true;
+            takeStamina(speedMul);
+        }
+        else if (Input.GetButtonUp("Sprint"))
+        {
+            speed /= speedMul;
+            IsRunning = false;
+        }
     }
 }
